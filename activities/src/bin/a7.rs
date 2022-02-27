e5d8a7cfe3bf3d36beffee61833114f5980f8c97
Jereld Lim <jereldlimjy@hotmail.com>
2022-02-28 01:55:31 +0800

    while loop, enums and struct

--- activities/src/bin/a7.rs
+++ activities/src/bin/a7.rs
@@ -10,4 +10,29 @@
 // * Use a match expression to determine which color
 //   name to print
 
-fn main() {}
+// * Use an enum with color names as variants
+enum Colour {
+  White,
+  Blue,
+  Grey,
+  Red,
+}
+
+// * Use a function to print the color name
+// * The function must use the enum as a parameter
+fn print_colour_name(colour: Colour) {
+  // * Use a match expression to determine which color
+  //   name to print
+  match colour {
+    Colour::White => println!("white"),
+    Colour::Blue => println!("blue"),
+    Colour::Grey => println!("grey"),
+    Colour::Red => println!("red"),
+  }
+}
+
+fn main() {
+  let my_colour = Colour::White;
+
+  print_colour_name(my_colour);
+}
--- activities/src/bin/a8.rs
+++ activities/src/bin/a8.rs
@@ -9,4 +9,38 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
-fn main() {}
+// * Use an enum to create different flavors of drinks
+enum Flavour {
+  Strawberry,
+  Grape,
+  Blueberry,
+  Orange,
+}
+
+// * Use a struct to store drink flavor and fluid ounce information
+struct Drink {
+  flavour: Flavour,
+  amount: f64,
+}
+
+// * Use a function to print out the drink flavor and ounces
+fn print_drink(drink: Drink) {
+  // * Use a match expression to print the drink flavor
+  match drink.flavour {
+    Flavour::Strawberry => println!("Strawberry"),
+    Flavour::Grape => println!("Grape"),
+    Flavour::Blueberry => println!("Blueberry"),
+    Flavour::Orange => println!("Orange"),
+  }
+
+  println!("Ounces: {}", drink.amount);
+}
+
+fn main() {
+  let my_drink = Drink {
+    flavour: Flavour::Grape,
+    amount: 4.0,
+  };
+
+  print_drink(my_drink);
+}
