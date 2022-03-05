488d9ad9120a1ba7b3f3053f6058b9a39c028167
Jereld Lim <jereldlimjy@hotmail.com>
2022-03-06 01:01:11 +0800

    advanced match, option type

--- activities/src/bin/a15.rs
+++ activities/src/bin/a15.rs
@@ -11,4 +11,66 @@
 // * Create one of each ticket and place into a vector
 // * Use a match expression while iterating the vector to print the ticket info
 
-fn main() {}
+// my attempt
+// struct TicketInfo {
+//   holder: String,
+//   price: i32,
+// }
+
+// enum Ticket {
+//   Backstage(TicketInfo),
+//   Vip(TicketInfo),
+//   Standard(i32),
+// }
+
+// fn main() {
+//   let tickets = vec![
+//     Ticket::Backstage(TicketInfo {
+//       holder: String::from("Justin"),
+//       price: 50,
+//     }),
+//     Ticket::Vip(TicketInfo {
+//       holder: "Jereld".to_owned(),
+//       price: 30,
+//     }),
+//     Ticket::Standard(10),
+//   ];
+
+//   for ticket in tickets {
+//     match ticket {
+//       Ticket::Standard(price) => println!("standard ticket price: {}", price),
+//       Ticket::Backstage(TicketInfo { holder, price }) => {
+//         println!("backstage ticket price: {}, holder: {}", price, holder)
+//       }
+//       Ticket::Vip(TicketInfo { holder, price }) => {
+//         println!("vip ticket price: {}, holder: {}", price, holder)
+//       }
+//     }
+//   }
+// }
+
+// instructor's answer
+// ordered alphabetically
+enum Ticket {
+  Backstage(f64, String),
+  Standard(f64),
+  Vip(f64, String),
+}
+
+fn main() {
+  let tickets = vec![
+    Ticket::Backstage(50.0, "Billy".to_owned()),
+    Ticket::Standard(15.0),
+    Ticket::Vip(30.0, "Amy".to_owned()),
+  ];
+
+  for ticket in tickets {
+    match ticket {
+      Ticket::Backstage(price, holder) => {
+        println!("Backstage ticket holder: {}, price: {}", holder, price)
+      }
+      Ticket::Standard(price) => println!("Standard ticket price: {}", price),
+      Ticket::Vip(price, holder) => println!("VIP ticket holder: {}, price: {}", holder, price),
+    }
+  }
+}
--- activities/src/bin/a16.rs
+++ activities/src/bin/a16.rs
@@ -8,7 +8,22 @@
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
 
-// * Use a struct containing the student's name and locker assignment
-// * The locker assignment should use an Option<i32>
+struct Locker {
+  student_name: String,
+  assignment: Option<i32>,
+}
 
-fn main() {}
+fn main() {
+  let my_locker = Locker {
+    student_name: "Jereld".to_owned(),
+    assignment: Some(32),
+  };
+
+  match my_locker {
+    Locker {
+      student_name,
+      assignment: Some(number),
+    } => println!("Locker {} assigned to {}", number, student_name),
+    Locker { student_name, .. } => println!("Locker assigned to {}", student_name),
+  }
+}
